089bbbbbccacc5f34eb0519753c2b291f4731874
jela3105 <jela3105@gmail.com>
2021-07-13 10:35:53 -0500

    Show hero screen by id with useParams hook

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <div>
       <h1>Hero Screen</h1>
