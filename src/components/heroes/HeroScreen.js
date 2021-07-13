3f2150297c71f50604c7938f9e5ebeeac85e618e
jela3105 <jela3105@gmail.com>
2021-07-13 10:50:54 -0500

    Add styles to HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { getHeroById } from "../../selectors/getHeroById";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
   const { heroId } = useParams();
   const hero = getHeroById(heroId);
 
@@ -10,9 +10,42 @@ export const HeroScreen = () => {
     return <Redirect to="/" />;
   }
 
+  const handleReturn = () => {
+    history.goBack();
+  };
+
   return (
     <div>
-      <h1>Hero Screen</h1>
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroId}.jpg`}
+          className="img-thumbnail"
+          alt={hero.superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance: </b>
+            {hero.first_appearance}
+          </li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{hero.characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          {" "}
+          Return{" "}
+        </button>
+      </div>
     </div>
   );
 };
