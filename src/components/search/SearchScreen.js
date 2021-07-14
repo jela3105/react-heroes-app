461d041cf821968c7229b7a97ad4e7db79857392
jela3105 <jela3105@gmail.com>
2021-07-14 18:26:11 -0500

    Create screen to search a hero

---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { heroes } from "../../data/heroes";
+import { HeroCard } from "../heroes/HeroCard";
+
+export const SearchScreen = () => {
+  const heroesFiltered = heroes;
+  return (
+    <div>
+      <h1>Search Screen</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Search form</h4>
+          <hr />
+          <form action="">
+            <input
+              type="text"
+              placeholder="Find you hero"
+              className="form-control"
+            />
+            <button
+              type="submit"
+              className="btn m-1 btn-block btn-outline-primary"
+            >
+              Search...
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          {heroesFiltered.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
